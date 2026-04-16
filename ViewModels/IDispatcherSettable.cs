7d45341e62ce3537af53ec2c4e8e57221ca44809
Ron Achin <rachin@ipgphotonics.com>
2026-04-16 16:53:43 -0400

    Checking in the addition of the ViewModelBase class and the IDispatcherSettable interface.

---
+++ ViewModels/IDispatcherSettable.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) IPG Photonics Corporation, Inc.  All Rights Reserved.
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Threading;
+
+namespace WpfApp1.ViewModels
+{
+    interface IDispatcherSettable
+    {
+        public interface IDispatcherSettable
+        {
+            void SetDispatcher(Dispatcher dispatcher);
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) IPG Photonics Corporation, Inc.  All Rights Reserved.
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+namespace WpfApp1.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDispatcherSettable
+    {
+        private static Dispatcher? dispatcher;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public ViewModelBase(Dispatcher dispatcher)
+        {
+            ViewModelBase.dispatcher = dispatcher;
+        }
+        public ViewModelBase()
+        {
+        }
+
+        public void BeginInvoke(Action action, DispatcherPriority dispPrio = DispatcherPriority.Normal)
+        {
+            dispatcher?.BeginInvoke(action, dispPrio);
+        }
+
+        public void Invoke(Action action, DispatcherPriority dispPrio = DispatcherPriority.Normal)
+        {
+            if (dispatcher?.CheckAccess() ?? false)
+            {
+                action();
+            }
+            else
+            {
+                dispatcher?.Invoke(action, dispPrio);
+            }
+        }
+        public void SetDispatcher(Dispatcher dispy)
+        {
+            dispatcher = dispy;
+        }
+    }
+}
