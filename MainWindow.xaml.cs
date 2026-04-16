b52ef1bb3f6b0f327f918151046fabe7f000d165
Ron Achin <rachin@ipgphotonics.com>
2026-04-16 17:32:13 -0400

    Final Version.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿// Copyright (c) IPG Photonics Corporation, Inc.  All Rights Reserved.
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -19,6 +11,7 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModels.MainViewModel();
         }
     }
 }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) IPG Photonics Corporation, Inc.  All Rights Reserved.
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Timers;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Title { get; set; }
+        public string currentTime;
+        System.Timers.Timer timer;
+
+        public MainViewModel()
+        {
+            Title = "Clock!";
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Start();
+
+            SetTime();
+        }
+
+        private void SetTime()
+        {
+            CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            lock (currentTime)
+            {
+                SetTime();
+            }
+        }
+
+        public string CurrentTime
+        {
+            get
+            {
+                return currentTime;
+
+            }
+            set
+            {
+                if (currentTime != value)
+                {
+                    currentTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
